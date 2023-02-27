8e39ab1263b2b1b96fa81bfd5f8303bab503a363
Bruno Fernandes <brunofernandes100@hotmail.com.br>
2023-02-26 22:48:25 -0300

    Initial commit

---
+++ backend/src/test/java/com/fernandes/bruno/foidebase/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandes.bruno.foidebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
